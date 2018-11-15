833fa45abd3e0c7c56f3059cee257a3278102d68
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:43:25 +0200

    add a multiplication operation

--- calculator.h
+++ calculator.h
@@ -1,11 +1,6 @@
-#ifndef CALCULATOR_H
-#define CALCULATOR_H
-
 class Calculator
 {
     public:
-        int Add (double, double);
-        int Sub (double, double);
+        int Add (int, int);
+        int Mul (double, double);
 };
-
-#endif//CALCULATOR_H
